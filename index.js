68b05755f25a2558118f7dc5e3880072517a9a7c
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-07 15:16:44 -0700

    Ensures .json file is actually written in json

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // This is just allowing us to use the dataset from the other file. You can ignore this line.
 const UFO_SIGHTINGS = require("./nyc-ufo-sightings.json");
+
+
+console.log(calculateMinutes(660));
+
+function calculateMinutes(seconds)
+{
+    return Math.floor(seconds/60);
+}
+
+
+function getRandomSightings()
+{
+    return fetch()
+}
+
+
